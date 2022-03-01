6d1b5722783d3a9cee839af8dafdd019c8cda42b
Sunandh1999 <bsunandhbabu@gmail.com>
2022-03-01 17:26:19 +0530

    change data

---
+++ PrjGit/EmployeeDetails/Employee/EmployeeData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class EmployeeData
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public void GetEmployeeData()
+        {
+
+        }
+    }
+}
